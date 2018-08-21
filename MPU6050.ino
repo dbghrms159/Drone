2db333657eaab9cfc63383a8e552f9f189022c4d
Ho_Jam <38156821+dbghrms159@users.noreply.github.com>
2018-08-21 10:53:10 +0900

    Add files via upload

---
+++ MPU6050.ino
@@ -0,0 +1,76 @@
+#define dt 0.02
+#define RAD 180 /3.14139
+
+int index = 7;
+int filter[7];
+int cpy[7];
+
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+float angles[6];
+float target_angle = 0.0;
+float Kp = 1.0, Ki= 0.0 , Kd = 0.5;
+float inte, prev;
+float pidDT ;
+float t_new = 0.0, t_prev = millis();
+
+void  printOrinentation() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  angles[0] = (float)GyX/131.072*dt;
+  angles[1] = (float)GyY/131.072*dt;
+  //angles[2] = (float)GyZ/131.072*dt;
+  angles[3] = (float)atan(AcY/sqrt(pow(AcX,2)+pow(AcZ,2)))*RAD;
+  angles[4] = (float)atan(AcX/sqrt(pow(AcY,2)+pow(AcZ,2)))*RAD;
+  //angles[5] = (float)GyX/131.072*dt;
+ 
+  angle = (0.96*(angle+angles[0]))+(0.04*angles[3]);
+  for(int a = index-1; a > 0; a--){
+    cpy[a] = filter[a] = filter[a-1];
+  }
+  filter[0] = cpy[0] = angle;
+  posSort(cpy);
+  angle = filter[3]= cpy[3];
+  
+  t_new = millis();
+  pidDT = (t_new - t_prev)/1000.0;
+  t_prev = t_new;
+  
+  PID(target_angle,(float)angle,prev,inte,resultAngle);
+  Serial.println(resultAngle);
+  
+}
+
+void PID(float& set, float input,float& prev, float& i, float& out){
+  float error;
+  float dInput;
+  float p, d;
+
+  error = set - input;
+  dInput = input - prev;
+  prev = input;
+
+  p = Kp * error;
+  i += Ki * error * pidDT;
+  d = Kd * (dInput / pidDT);
+  out = p + i + d;
+}
+
+void posSort(int sorts[]){
+  for(int i= 0;i<index-1;i++)
+    for(int j = 1; j< index-i; j++)
+      if(sorts[j-1]>sorts[j]){
+        int cpy= sorts[j-1];
+        sorts[j-1] = sorts[j];
+        sorts[j] = cpy;
+      }  
+}
---
+++ drone.ino
@@ -0,0 +1,99 @@
+#include<Wire.h>
+
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+
+int angle = 0;
+int input_Angle = 0;
+int angle_pev = 0;
+int left = 3;
+int right = 4;
+int analogAngle = A0;
+int pwmleft , pwmright;
+int input_Index = 0;
+
+float resultAngle= 0.0;
+
+char input_char[3];
+  
+void setup() {
+  MPUSetUp();
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  pinMode(left, OUTPUT);
+  pinMode(right, OUTPUT);
+  digitalWrite(left,LOW);
+  digitalWrite(right,LOW);
+  pwmleft  = pwmright = 5;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int angleInput = map(analogRead(analogAngle), 0, 1023, -9, 9);
+  
+  printOrinentation();  //mpu를 이용하여 각도 계산 
+  
+  serial_Input();       //serial을 이용해 원하는 각도 입력
+  
+  PWM(pwmleft,pwmright); 
+}
+
+void serial_Input(){
+  if(Serial.available()){
+    while(Serial.available()){
+      input_char[input_Index] = Serial.read();   //char한글자씩 입력 받기에 char형 배열로 받아온다.
+      input_Index++;
+    }
+    input_Angle = atoi(input_char);               //char를 int형으로 변환
+  }
+  
+  for(int i = 0; i <= input_Index; i++){
+    input_char[i] = NULL;
+  }
+  input_Index = 0;
+}
+
+//MPU셋팅
+void MPUSetUp(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);  
+}
+
+//좌 우 모터 pwm 제어
+void PWM(int leftdelay,int rightdelay){
+  Serial.print(leftdelay);
+  Serial.print("/");
+  Serial.println(rightdelay);
+  if(angle > input_Angle){
+    pwmleft -= 1;
+    pwmright += 1; 
+  }else if(angle < input_Angle){
+    pwmleft += 1;
+    pwmright -= 1;
+  }
+
+  if(pwmleft < 1) pwmleft = 1;
+  if(pwmleft > 9) pwmleft = 9;
+  if(pwmright < 1)  pwmright = 1;
+  if(pwmright > 9)  pwmright = 9;
+  
+  motorCycle(left, pwmleft);
+  motorCycle(right, pwmright);
+  
+  Serial.print("left : ");
+  Serial.println(leftdelay);
+  Serial.print("right : ");
+  Serial.println(rightdelay);
+  angle_pev = angle;
+}
+
+// 모터제어
+void motorCycle(int angles,int delayTime){
+  digitalWrite(angles, HIGH);
+  delay(delayTime);
+  digitalWrite(angles, LOW);
+  delay(50 - delayTime);
+}
+
